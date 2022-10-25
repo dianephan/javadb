908e49303d2350a1145e18ad1997d5c2a8a5c939
dIANE phan <dianephan12@gmail.com>
2022-10-25 15:10:52 -0700

    writing unit tests

--- src/main/java/com/example/javadb/repository/QuoteRepository.java
+++ src/main/java/com/example/javadb/repository/QuoteRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 // can add new funcs like finding quotes with particular word
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
+
     @Query("SELECT q FROM Quote q WHERE q.quote LIKE %?1%")
     List<Quote> getContainingQuote(String word);     // dont need to define method bc in repository
 }
